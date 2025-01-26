60414795d948c27fa389014364e11f856d9874f2
MAZER-shadow <devatyhpasa2@gmail.com>
2025-01-27 00:16:32 +0300

    updateReader

--- java/lab5/src/main/java/org/example/Main.java
+++ java/lab5/src/main/java/org/example/Main.java
@@ -1,27 +1,32 @@
 package org.example;
 
 
+import org.example.entity.Coordinates;
 import org.example.entity.Person;
 import org.example.io.ReaderFile;
 import org.example.io.WriterJson;
 
+import java.time.Period;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        WriterJson writerJson = new WriterJson();
-        List<Person> people = new ArrayList<>();
-        people.add(new Person("David"));
-        people.add(new Person("Mark"));
-        writerJson.writeToJson(people,"FileWithPerson.json");
-        ReaderFile reader = new ReaderFile();
-        List<Person> personList = reader.readJson("FileWithPerson.json");
-        System.out.println(personList.get(0).getName());
-
+        WriterJson<Coordinates> writerJson = new WriterJson<>();
+        List<Coordinates> people = new ArrayList<>();
+        people.add(new Coordinates());
+        people.add(new Coordinates());
+        writerJson.writeToJson(people,"src\\main\\resources\\FileWithPerson.json");
+        ReaderFile<Coordinates> reader = new ReaderFile<>(Coordinates.class);
+        List<Coordinates> personList = reader.readJson("src\\main\\resources\\FileWithPerson.json");
+        System.out.println(personList.get(0).getX());
 
 
 
     }
 }
+
+
+
+
--- java/lab5/src/main/java/org/example/io/ReaderFile.java
+++ java/lab5/src/main/java/org/example/io/ReaderFile.java
@@ -2,25 +2,32 @@ package org.example.io;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
-import org.example.entity.Person;
-import org.example.io.JsonReader;
+
 
 import java.io.*;
 import java.lang.reflect.Type;
 import java.util.List;
 
 
-public class ReaderFile<T> implements JsonReader<T> {
+public class ReaderFile<T> implements JsonReader<T>{
+    private final Type listType;
+
+    public ReaderFile(Class<T> clazz) {
+        this.listType = TypeToken.getParameterized(List.class, clazz).getType();
+    }
+
     @Override
     public List<T> readJson (String path){
         try (BufferedReader reader = new BufferedReader(new FileReader(path)))  {
             Gson gson = new Gson();
-            Type personListType = new TypeToken<List<Person>>() {}.getType();
-            List<T> person = gson.fromJson(reader, personListType);
-            return person;
+            return gson.fromJson(reader, listType);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
+
+
 }
+
+
 
